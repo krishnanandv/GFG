a1f1be8c523f561ce9a8a71e3e5ef9baf3f79871
Krishnanand V <krishvn@amazon.com>
2022-01-09 14:37:25 +0530

    Again created file - index of first occurance

--- Searching/index of first occurance in sorted (2).py
+++ Searching/index of first occurance in sorted (2).py
@@ -12,12 +12,15 @@ def first_occur(arr,low,high,x):
     if low > high:
         return -1
     mid = (low+high)//2
-    if x>arr[mid]:
-        return first_occur()
-    return(-1)
-
-
-
+    if x > arr[mid]:
+        return first_occur(arr,mid+1,high,x)
+    elif x < arr[mid]:
+        return first_occur(arr,low, mid-1, x)
+    else:
+        if (mid==0 or arr[mid-1]!=arr[mid]):
+            return mid
+        else:
+            first_occur(arr,low,mid-1,x)
 
 #Test Case - Naive Solution
 # arr = [1,10,10,20,30,40,50,100]
@@ -30,8 +33,12 @@ def first_occur(arr,low,high,x):
 #Solution
 arr = [1,10,10,20,30,40,50,100]
 x=40
-print(first_occur(arr,x))
-# arr = [10,20,30]
-# x=15
-# print(first_occur(arr,x))
-
+print(first_occur(arr,0, 7,x))
+#Test 2
+arr = [10,20,30]
+x=15
+print(first_occur(arr,0,2,x))
+#Test 3
+arr=[4,5]
+x=4
+print(first_occur(arr,0,1,x))
