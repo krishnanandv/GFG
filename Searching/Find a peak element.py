73ef4543ab3e4712d72176dca54f648d8d3ebe30
Krishnanand V <iamkrishji@gmail.com>
2022-04-17 20:55:57 +0530

    Find a peak element

--- Searching/Find a peak element.py
+++ Searching/Find a peak element.py
@@ -11,23 +11,25 @@ def naivePeak(arr):
     for i in range(1,n-1):
         if (arr[i]>arr[i-1] and arr[i]>arr[i+1]):
             return arr[i]
+#arr=[1,20]
 #arr=[1,20,24,11,9,17,21,55,1,2,0]
 #print(naivePeak(arr))
 
 #Efficient solution
 def FindPeak(arr):
     n=len(arr)
+    #print(n)
     low = 0
     high = n-1
     while(low<=high):
         mid = (low+high)//2
-        if ((mid==0 or arr[mid-1]<=arr[mid]) and (mid==n-1 or (arr[mid]>=arr[mid+1]))):
+        if ((mid==0 or arr[mid-1]<=arr[mid]) and (mid==n-1 or (arr[mid+1]<=arr[mid]))):
             return arr[mid]
-        elif (arr[mid-1]>arr[mid]):
+        elif (mid>0 and arr[mid-1]>=arr[mid]):
             high = mid-1
         else:
             low = mid+1
     return -1
 
-arr=[1,20,24,11,9,17,21,19,1,2,0]
+arr=[1,20]
 print(FindPeak(arr))
