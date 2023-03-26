bd3d103c1d9072b5793259cda9468db06b26b799
Krishnanand V <iamkrishji@gmail.com>
2023-03-26 19:06:28 +0530

    Largest number in list

---
+++ DSA_Python/03 List/02 Largest number in a list.py
@@ -0,0 +1,17 @@
+def large_num(arr):
+    large = arr[0]
+    if len(arr) > 1 :
+        for elem in arr:
+            if elem > large:
+                large = elem
+    return large
+
+
+arr = [4, 5, 3, 2, 11]
+print(large_num(arr))
+
+arr = [40]
+print(large_num(arr))
+
+arr = [30, 30, 30]
+print(large_num(arr))
